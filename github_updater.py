ebe5e36d0a67578228617e09a379825eef6d196b
CHSIVASANTHOSH <55751509+CHSIVASANTHOSH@users.noreply.github.com>
2023-02-11 20:03:01 +0530

    updated the code

--- github_updater.py
+++ github_updater.py
@@ -1,52 +1,60 @@
+# Plugin meta-data
+__plugin_name__ = "Klipper Config Updater"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Updates Klipper printer.cfg from Github repo"
+__plugin_pythoncompat__ = ">=3.7,<4"
+
+# Plugin imports
 import octoprint.plugin
 import requests
+import shutil
 import os
+import threading
 
-class GitHubUpdaterPlugin(octoprint.plugin.StartupPlugin):
-    def __init__(self):
-        self._repo_url = "https://github.com/FracktalWorks/Klipper_CFG/raw/main/Twin%20Dragon_IDEX.cfg"  # replace with your own repo URL
-        self._config_file_path = "/home/pi/printer.cfg"  # replace with the path to your Klipper config file
-
+class KlipperConfigUpdaterPlugin(octoprint.plugin.StartupPlugin,
+                                  octoprint.plugin.TemplatePlugin,
+                                  octoprint.plugin.AssetPlugin):
     def on_after_startup(self):
-        self._current_commit_sha = self._get_current_commit_sha()
-        self._schedule_update_check()
+        self._logger.info("Klipper Config Updater started")
+        self.start_update_thread()
 
-    def _get_current_commit_sha(self):
-        with open(self._config_file_path, "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                if line.startswith("# commit "):
-                    return line[len("# commit "):].strip()
+    def get_assets(self):
+        return {
+            "js": ["js/klipper_config_updater.js"]
+        }
 
-    def _download_new_config_file(self):
-        r = requests.get(self._repo_url + "/raw/main/printer.cfg")
-        if r.status_code == requests.codes.ok:
-            with open(self._config_file_path, "w") as f:
-                f.write(r.text)
-            return True
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
+
+    def start_update_thread(self):
+        self.update_thread = threading.Timer(15 * 24 * 60 * 60, self.update_config)
+        self.update_thread.start()
+
+    def update_config(self):
+        self._logger.info("Checking for updates...")
+        url = "https://github.com/FracktalWorks/Klipper_CFG/raw/main/Twin%20Dragon_IDEX.cfg"
+        r = requests.get(url, allow_redirects=True)
+        if r.status_code == 200:
+            filename = os.path.join(os.path.expanduser("~"), "printer.cfg")
+            with open(filename, "wb") as f:
+                f.write(r.content)
+            self._logger.info("Printer config updated from Github repo")
         else:
-            return False
+            self._logger.info("Printer config update failed. HTTP status code: %d" % r.status_code)
 
-    def _schedule_update_check(self):
-        threading.Timer(3600, self._check_for_updates).start()  # check every hour
+        self.start_update_thread()
 
-    def _check_for_updates(self):
-        try:
-            r = requests.get(self._repo_url + "/commits/main")
-            if r.status_code == requests.codes.ok:
-                commit_sha = r.json()["sha"]
-                if commit_sha != self._current_commit_sha:
-                    if self._download_new_config_file():
-                        self._current_commit_sha = commit_sha
-                        self._show_success_message()
-            else:
-                self._show_error_message("Unable to check for updates")
-        except Exception as e:
-            self._show_error_message("Error checking for updates: {}".format(str(e)))
-        self._schedule_update_check()
+__plugin_name__ = "Klipper Config Updater"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Updates Klipper printer.cfg from Github repo"
+__plugin_pythoncompat__ = ">=3.7,<4"
 
-    def _show_success_message(self):
-        self._plugin_manager.send_plugin_message(self._identifier, {"type": "success", "message": "Configuration file updated"})
-
-    def _show_error_message(self, message):
-        self._plugin_manager.send_plugin_message(self._identifier, {"type": "error", "message": message})
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = KlipperConfigUpdaterPlugin()
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
